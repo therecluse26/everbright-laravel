ce2aa3cd39a665a30b0f7521aa161bf252c82d65
Brad Magyar <braddmagyar@gmail.com>
2018-03-05 12:13:16 -0500

    Rolled back to Bootstrap v3

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,4 +1,4 @@
-    <nav class="navbar navbar-default navbar-static-top">
+<nav class="navbar navbar-default navbar-static-top">
         <div class="container">
             <div class="navbar-header">
 
@@ -74,4 +74,4 @@
                 </ul>
             </div>
         </div>
-    </nav>
+</nav>
